4de8072364166159efb8f02e6b043fd5428dba5d
bala_jkpm <30350603+BalaKumaran1998@users.noreply.github.com>
2019-11-21 15:49:14 +0530

    Armstrong Number Using Function

---
+++ Armstrong Number.py
@@ -0,0 +1,13 @@
+n=int(input())
+def arms(n):
+  sum = 0
+  temp = n
+  while temp > 0:
+    digit=temp % 10
+    sum=digit ** 3
+    temp //= 10
+  if (n == sum):
+   print(n,"is  Armstrong number")
+  else:
+   print(n,"is not  Armstrong number")
+arms(n)
