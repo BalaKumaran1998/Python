fc8dd3157a4ef9d4a7bc57f19e2f6d3e46f25de7
bala_jkpm <30350603+BalaKumaran1998@users.noreply.github.com>
2019-12-09 15:03:35 +0530

    Bubble sort using list of numbers

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,7 +1,7 @@
 l=[9,2,10]
-n=len(l)
-for i in range(0,n):
-  for j in range(0,n):
+nm=len(l)
+for i in range(0,nm):
+  for j in range(0,nm):
     if(l[i]< l[j]):
       l[i],l[j]=l[j],l[i]
       print(l)
